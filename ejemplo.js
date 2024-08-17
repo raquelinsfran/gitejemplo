22371b2ff52777f8b61195e7db18afeec3cdd32a
Your username <raquelinsfran17@gmail.com>
2024-08-16 23:19:00 -0400

    Primer commit

---
+++ ejemplo.js
@@ -0,0 +1,9 @@
+console.log('Hello, World!');
+
+// Declarar dos variables y realizar la suma
+let a = 5;
+let b = 3;
+let sum = a + b;
+
+// Imprimir el resultado de la suma
+console.log('La suma de', a, 'y', b, 'es:', sum);
